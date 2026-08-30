e1c70ccdead68c9d7971b067c5e90346736a5663
fullytested-1 <atextnow837@gmail.com>
2026-08-30 17:15:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -237,14 +237,14 @@ const baseConfig = {
     "78c84c62b32a88e86daf87dd509a657a",
   ],
   RG: process.env.RG || "919074309534-1632403322@g.us,120363116963909366@g.us",
-  BOT_INFO: process.env.BOT_INFO || "𝖱𝖺𝗀𝖺𝗇𝗈𝗋𝗄;𝖱𝗒𝗓𝖾𝗇;default",
+  BOT_INFO: process.env.BOT_INFO || "𝚁𝙾𝙼𝙰-𝙼𝙳;𝐀𝐍𝐒𝐀𝐃;default",
   RBG_KEY: process.env.RBG_KEY || "",
   ALLOWED: process.env.ALLOWED || "91,94,2",
   NOT_ALLOWED: process.env.NOT_ALLOWED || "852",
   CHATBOT: process.env.CHATBOT || "off",
   HANDLERS: process.env.HANDLERS || ".,",
-  STICKER_DATA: process.env.STICKER_DATA || "Raganork",
-  BOT_NAME: process.env.BOT_NAME || "Raganork",
+  STICKER_DATA: process.env.STICKER_DATA || "𝚁𝙾𝙼𝙰-𝙼𝙳",
+  BOT_NAME: process.env.BOT_NAME || "𝚁𝙾𝙼𝙰-𝙼𝙳",
   AUDIO_DATA:
     process.env.AUDIO_DATA === undefined || process.env.AUDIO_DATA === "private"
       ? "default"
