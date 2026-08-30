d1ad0e9984aaa1bce73870c1ef16e9815948d46f
fullytested-1 <atextnow837@gmail.com>
2026-08-30 17:39:06 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -70,6 +70,6 @@ Module(
     let sent_msg = await message.sendReply("*❮ ᴛᴇsᴛɪɴɢ ᴘɪɴɢ ❯*");
     const diff = process.hrtime(start);
     const ms = (diff[0] * 1e3 + diff[1] / 1e6).toFixed(2);
-    await message.edit("*𝙻𝚊𝚝𝚎𝚗𝚌𝚢: " + 𝙼𝚂 + " _𝙼𝚂_*", message.jid, sent_msg.key);
+    await message.edit("*𝙻𝚊𝚝𝚎𝚗𝚌𝚢: " + ms + " _𝙼𝚂_*", message.jid, sent_msg.key);
   }
 );
