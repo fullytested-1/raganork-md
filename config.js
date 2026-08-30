4422faee4a3841456500aad02a1c648f19c5d1ca
fullytested-1 <atextnow837@gmail.com>
2026-08-30 17:19:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -188,7 +188,7 @@ const baseConfig = {
   VERSION,
   ALIVE:
     process.env.ALIVE ||
-    "_I am alive! (use .setalive help for custom alive msg)_",
+    "_𝙷𝙴𝚈 𝚆𝙴𝙻𝙲𝙾𝙼𝙴 𝚃𝙾 𝚁𝙾𝙼𝙰-𝙼𝙳 (use .setalive help for custom alive msg)_",
   BLOCK_CHAT: process.env.BLOCK_CHAT || "",
   PM_ANTISPAM: convertToBool(process.env.PM_ANTISPAM) || "",
   ALWAYS_ONLINE: convertToBool(process.env.ALWAYS_ONLINE) || false,
